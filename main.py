ec8fa0501329c7f6b033633886ba025d635239c1
Sergey Yur <sergeyyur@Sergeys-MacBook-Pro.local>
2019-07-16 08:36:18 +0300

    Should count stars

--- main.py
+++ main.py
@@ -3,9 +3,11 @@ import pytesseract
 import numpy as np
 import os, fnmatch, ntpath, re
 
-from PIL import Image
+from PIL import Image, ImageDraw
 
-_backgroundColors = ['#25222d', '#312c40']
+_backgroundColors = ('#25222d', '#312c40')
+_starColors = ('#bbb2a9', '#b4c4e6', '#f3ef00')
+_starColorsRgb = ((167, 168, 162), (180, 196, 230), (243, 250, 45))
 
 _tmpFolder = 'temp/'
 _heroIconFolder = 'img/hero-icons/'
@@ -73,7 +75,6 @@ def getPlayerCrop(file_name):
         
         if largest[3] * largest[2] < w * h:
             largest = (x, y, w, h)
-            print(largest)
     
     x, y, w, h = largest
     cv2.rectangle(img, (0, y), (round(fullW / 8), y + h), (255, 0, 255), 2)
@@ -104,32 +105,55 @@ def getPlayerLevel(imgName):
             if loc[0].size > 0:
                 return re.findall('\d+', filename)[0]
 
+def getStarsByColor(bigImgName, frame):
+    img = Image.open(bigImgName)
+    rgb_img = img.convert('RGB')
+    x, y, w, h = frame
+    pixel = (int(x + round(w / 2)), int(y + round(h * 1.6)))
+    r, g, b = rgb_img.getpixel(pixel)
+
+    img.close()
+    rgb_img.close()
+
+    print((r, g, b))
+    print(pixel)
+    for rgb in _starColorsRgb:
+        delta = abs(rgb[0] - r) / 3 + abs(rgb[1] - g) / 2 + abs(rgb[2] - b)
+        if delta < 70:
+            return _starColorsRgb.index(rgb) + 1
+        print(delta)
+    
+    # its likely that we hit between 2 stars 
+    return 2
+
+def checkPointWithPrev(pt, prevPts):
+    for prev in prevPts:
+        if abs(prev[0] - pt[0]) < 10 and  abs(prev[1] - pt[1]) < 10:
+            return 0
+    return 1
+
 if __name__ == "__main__":
     method = cv2.TM_SQDIFF
-    smallImgName = OsFind('Axe*', _heroIconFolder)
+    smallImgName = OsFind('Queen*', _heroIconFolder)
+
+    largeImgName = 'score4.png'
     
     small_image = cv2.imread(cropSome(smallImgName))
-    #large_image = cv2.imread('score.png')
-    large_image = cv2.imread('score3.jpg')
-    #large_image = cv2.imread('score2.jpeg')
+    large_image = cv2.imread(largeImgName)
 
     w, h = small_image.shape[:-1]
 
     res = cv2.matchTemplate(large_image, small_image, cv2.TM_CCOEFF_NORMED)
     threshold = .6
     loc = np.where(res >= threshold)
-    prev = 0
+    prevPts = []
     for pt in zip(*loc[::-1]):  # Switch collumns and rows
-        if prev == 0:
-            prev = pt
-        else:
-            if abs(prev[0] - pt[0]) < 10 and  abs(prev[1] - pt[1]) < 10:
-                continue
-            else:
-                prev = pt
-        cv2.rectangle(large_image, pt, (pt[0] + h, pt[1] + w), (0, 255, 0), 2)
-        print("row:")
-        print(pt)
+        if checkPointWithPrev(pt, prevPts):
+            prevPts.append(pt)
+            cv2.rectangle(large_image, pt, (pt[0] + h, pt[1] + w), (0, 255, 0), 2)
+            print("row:")
+            print(pt)
+            print(str(getStarsByColor(largeImgName, (pt[0], pt[1], w, h))) + ' stars')
 
     cv2.imwrite('result.png', large_image)
 
