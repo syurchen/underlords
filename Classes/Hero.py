2103cbd374e5b0b285c5b734276c964ff01b89e7
Sergey Yur <sergeyyur@sergeys-mbp.tpad>
2019-07-17 17:07:58 +0300

    hero class work

--- Classes/Hero.py
+++ Classes/Hero.py
@@ -1,10 +1,37 @@
 class Hero:
-    name = ''
-    rarity = ''
+    _name = ''
+    _cost = 1
+    _star = 1
 
     _iconPath = ''
+    _iconFolder = ''
 
-    def __init__(self, name, rarity):
-        self.name = name
-        self.rarity = rarity
+    def __init__(self, name, cost, iconFolder, star = 1):
+        self._name = name
+        self._cost = cost
+        self._star = star
+        self._iconFolder = iconFolder
+
+    def findIcon(self):
+        if self._iconPath != '':
+            return self._iconPath
+        else:
+            # @TODO
+            return self._iconPath
+    
+    def getIcon(self):
+        return self._iconPath
+
+    def getCost(self):
+        return self._cost
+
+    def getStar(self):
+        return self._star
+
+    def tokenize(self):
+        count = self._star
+        self._star = 1
+        return count
+
+
 
